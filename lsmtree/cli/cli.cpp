5839437ead407e2312cb086b158ce819b53ee624
Xiangyu Wang <wxyucs@gmail.com>
2022-05-31 23:28:57 +0800

    update CLI

--- lsmtree/cli/cli.cpp
+++ lsmtree/cli/cli.cpp
@@ -15,8 +15,42 @@ public:
         engine_ = nullptr;
     }
 
+    void Open(string dir) {
+        if (not filesystem::exists(dir)) {
+            throw runtime_error("error: " + dir + " not exists");
+        }
+        engine_ = new Engine(dir, 4096);
+    }
+
+    void Close() {
+        delete engine_;
+        engine_ = nullptr;
+    }
+
+    void Set(vector<string> args) {
+        if (not engine_) {
+            throw runtime_error("error: should open datadir first");
+        }
+        engine_->Set(args[0], args[1]);
+    }
+
+    void Get(string key) {
+        if (not engine_) {
+            throw runtime_error("error: should open datadir first");
+        }
+        cout << engine_->Get(key) << endl;
+    }
+
+    void Keys() {
+        if (not engine_) {
+            throw runtime_error("error: should open datadir first");
+        }
+        vector<string> keys = engine_->Keys();
+        cout << DumpKeys(keys) << endl;
+    }
+
     void Serve() {
-        cout << "welcome" << endl;
+        cout << "welcome!" << endl;
         string line;
         while (cout << "lsmtree cli> " && getline(cin, line))  {
             vector<string> args = Parse(line);
@@ -25,56 +59,32 @@ public:
             }
 
             string cmd = args[0];
-            if (cmd == ".open") {
-                if (args.size() != 2) {
-                    Error("format error, usage: .open <datadir>");
-                    continue;
+            try {
+                if (cmd == ".open") {
+                    CheckNumArguments(args, 2,
+                            "error: format error, usage: .open <datadir>");
+                    Open(args[1]);
+                } else if (cmd == ".close") {
+                    CheckNumArguments(args, 1,
+                            "error: format error, usage: .close");
+                    Close();
+                } else if (cmd == ".set") {
+                    CheckNumArguments(args, 3,
+                            "error: format error, usage: .set <key> <value>");
+                    Set({args[1], args[2]});
+                } else if (cmd == ".get") {
+                    CheckNumArguments(args, 2,
+                            "error: format error, usage: .get <key>");
+                    Get(args[1]);
+                } else if (cmd == ".keys") {
+                    CheckNumArguments(args, 1,
+                            "error: format error, usage: .keys");
+                    Keys();
+                } else {
+                    cout << "unsupported commands" << endl;
                 }
-                if (not filesystem::exists(args[1])) {
-                    Error(args[1] + " not exists");
-                    continue;
-                }
-                engine_ = new Engine(args[1], 4096);
-            } else if (cmd == ".close") {
-                delete engine_;
-                engine_ = nullptr;
-            } else if (cmd == ".set") {
-                if (args.size() != 3) {
-                    Error("format error, usage: .set <key> <value>");
-                    continue;
-                }
-                if (not engine_) {
-                    Error("should open datadir first");
-                    continue;
-                }
-                engine_->Set(args[1], args[2]);
-            } else if (cmd == ".get") {
-                if (args.size() != 2) {
-                    Error("format error, usage: .get <key>");
-                    continue;
-                }
-                if (not engine_) {
-                    Error("should open datadir first");
-                    continue;
-                }
-                auto value = engine_->Get(args[1]);
-                cout << value << endl;
-            } else if (cmd == ".keys") {
-                if (args.size() != 1) {
-                    Error("format error, usage: .keys");
-                    continue;
-                }
-                if (not engine_) {
-                    Error("should open datadir first");
-                    continue;
-                }
-                vector<string> keys = engine_->Keys();
-                // cout << DumpKeys(keys) << endl;
-                for (auto &key : keys) {
-                    cout << "\"" << key << "\"" << endl;
-                }
-            } else {
-                cout << "unsupported commands" << endl;
+            } catch (runtime_error &e) {
+                cout << e.what() << endl;
             }
         };
 
@@ -96,12 +106,15 @@ private:
         if (not line.empty()) {
             ret.push_back(line);
         }
-
         return ret;
     }
 
-    void Error(string msg) {
-        cout << "error: " << msg << endl;
+    void CheckNumArguments(vector<string> &args, 
+            size_t numArgs, 
+            string errmsg) {
+        if (args.size() != numArgs) {
+            throw runtime_error(errmsg);
+        }
     }
 
     void DumpArgs(vector<string> args) {
@@ -119,7 +132,7 @@ private:
         ss << "[";
         ss << Wrap(keys[0]);
         for (size_t i = 1; i < keys.size(); ++i) {
-            cout << "," << Wrap(keys[i]);
+            ss << "," << Wrap(keys[i]);
         }
         ss << "]";
         return ss.str();
